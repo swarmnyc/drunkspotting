76d9f320c15eff59706f7886a52ac05761f53ac2
Adam Ritenauer <aritenauer@dxysolutions.com>
2013-03-08 01:16:22 -0500

    Added unit tests

---
+++ client/ios/DrunkSpottingTest/DrunkSpottingTest.h
@@ -0,0 +1,13 @@
+//
+//  DrunkSpottingTest.h
+//  DrunkSpottingTest
+//
+//  Created by Adam Ritenauer on 08.03.13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DrunkSpottingTest : SenTestCase
+
+@end
