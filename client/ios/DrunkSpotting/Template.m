d374047e74da4e37b23854e77b740c97d61ef4a3
Somya Jain <somyaj@gmail.com>
2013-03-08 01:12:07 -0500

    added Template service

---
+++ client/ios/DrunkSpotting/Template.m
@@ -0,0 +1,20 @@
+//
+//  Created by somya on 3/8/13.
+//
+//
+
+
+#import "Template.h"
+
+@implementation Template
+@synthesize description = m_description;
+@synthesize id = m_id;
+@synthesize latitude = m_latitude;
+@synthesize longitude = m_longitude;
+@synthesize rating = m_rating;
+@synthesize rating_count = m_rating_count;
+@synthesize time_posted = m_time_posted;
+@synthesize title = m_title;
+@synthesize url = m_url;
+
+@end
