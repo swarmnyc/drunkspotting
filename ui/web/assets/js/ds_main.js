6b98777eedb00f20ac5e4ff5f2aa47086d7102c7
Rui Da Costa <rui.dacosta@gmail.com>
2013-03-14 16:02:11 -0400

    CSS, Editor, Canvas
    Fixed a slew of issues with some more CSS, added a properly styled
    editor and fixed some canvas issues on mobile. Also removed more
    Bootstrap files (not needed).

--- ui/web/assets/js/ds_main.js
+++ ui/web/assets/js/ds_main.js
@@ -33,7 +33,7 @@ drunkspotting.load_images = function(){
 };
 
 drunkspotting.upload_ajax = function(){
-  
+  drunkspotting.loading_start();
   // Kill image reloading timeout
   clearTimeout(window.ds_reloader);
   
@@ -42,68 +42,128 @@ drunkspotting.upload_ajax = function(){
   data.append('file', $('#data')[0].files[0]);
   
   $.ajax({
-    url : 'http://api.drunkspotting.com/upload_template',
+    url : 'http://dev.syphen.org/test.php',
     type : "POST",
     data : data,
+    dataType: 'JSON',
     processData : false,
     contentType : false,
     success : function(data){
-
       // Init canvas
       var newUrl = data.url.replace('http://drunkspotting.blob.core.windows.net/', 'http://drunkspotting.com/');
       drunkspotting.init_drawing(newUrl);
-
-      $('#upload-panel').hide();
-      $('#edit-panel').show();
+      
+      $(document.body).addClass('edit');
+      
+      drunkspotting.loading_stop();
     },
     error : function (){}
   });
-  
-  // On upload image, hide most shit on the page until they save the drawing
-  $('.page-heading img').hide();
-  $('.page-heading').css({
-    height : '60px',
-    padding : '15px'
-  });
-  $('.marketing-desc').hide();
-  $('.upload-btn-container').hide();
-  $('#lushes-box').hide();
-  $('#footer-marketing').hide();
 };
+
+$(window).resize(function(){
+	drunkspotting.fix_canvas();
+});
+
+drunkspotting.fix_canvas = function(){
+	var canvasWidth, canvasHeight;
+	$('#sketch').css('width', '100%');
+	var maxWidth = $('#sketch').width()-22;
+	$('#sketch').css('width', 'auto');
+	var maxHeight = $(document.body).height() - $('#tools').outerHeight()-12;
+	var imgWidth = parseInt($('#sketch').attr('img_width'));
+	var imgHeight = parseInt($('#sketch').attr('img_height'));
+	if(imgWidth/imgHeight >= maxWidth/maxHeight){
+		//use width as base
+		if((maxWidth/imgWidth) * imgHeight >= maxHeight){
+			canvasHeight = maxHeight;
+			canvasWidth = (maxHeight/imgHeight) * imgHeight
+		}
+		else {
+			canvasHeight = (maxWidth/imgWidth) * imgHeight;
+			canvasWidth = maxWidth;
+		}
+	}
+	else {
+		//use height as base
+		if((maxHeight/imgHeight) * imgWidth >= maxWidth){
+			canvasWidth = maxWidth;
+			canvasHeight = (maxWidth/imgWidth) * imgWidth;
+		}
+		else {
+			canvasWidth = (maxHeight/imgHeight) * imgWidth;
+			canvasHeight = maxHeight;
+		}
+	}
+	
+	$('#sketch')[0].width = Math.floor(canvasWidth);
+	$('#sketch')[0].height = Math.floor(canvasHeight);
+	
+	$('#sketch').sketch();
+}
 
 drunkspotting.init_drawing = function(image_url){
 
   // set up the drawing canvas
   window.image = $('<img />');
-  window.image.on('onload', function () {
-    $('#tools_sketch')[0].height = window.image[0].height;
-    $('#tools_sketch')[0].width = window.image[0].width;
+  window.image.on('load', function () {
+    $('#sketch').attr('img_width', window.image[0].width);
+    $('#sketch').attr('img_height', window.image[0].height);
+    $('#sketch').css('background-image', 'url(' + window.image[0].src + ')');
+    drunkspotting.fix_canvas();
   });
   window.image.attr('src', image_url);
-
-  $('#tools_sketch').css('background', 'url(' + image_url + ') no-repeat');
-
-  $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function() {
-    $('#edit-panel .tools').append("<a href='#tools_sketch' data-color='" + this + "' style='width: 10px; background: " + this + ";'>" + this + "</a>");
+  
+  var colorpopup = $('<div id="color_popup"/>');
+  colorpopup.appendTo('#tools').hide();
+  $.each(['#ff0000', '#ff8000', '#ffff00', '#80ff00', '#00ff00', '#00ff80', '#00ffff', '#0080ff', '#0000ff', '#8000ff', '#ff00ff', '#ff0080', '#000000', '#808080', '#ffffff'], function() {
+    var link = $('<a href="#sketch" data-color="' + this + '"><span style="background: ' + this + ';"></span></a>');
+    link.unbind('click').bind('click', function(){
+    	$('#my_color span').css('background-color',$(this).find('span').css('background-color'));
+    	$('#color_popup').fadeOut(250);
+    });
+    colorpopup.append(link);
   });
-  $.each([3, 5, 10, 15], function() {
-    $('#edit-panel .tools').append("<a href='#tools_sketch' data-size='" + this + "' style='background: #ccc'>" + this + "</a> ");
+  $('#my_color').unbind('click').bind('click', function(){
+  	  $('#size_popup').fadeOut(250);
+	  $('#color_popup').fadeIn(500);
   });
-
-  $('#tools_sketch').sketch();
+  $('#my_color span').css('background-color','#000000');
+  
+  var sizepopup = $('<div id="size_popup"/>');
+  sizepopup.appendTo('#tools').hide();
+  $.each([2, 6, 10, 16, 20], function() {
+    var link = $('<a href="#sketch" data-size="' + this + '" class="size_'+this+'"><span></span></a>');
+    link.unbind('click').bind('click', function(){
+    	$('#my_size').attr('data-size', $(this).attr('data-size'));
+    	$('#size_popup').fadeOut(250);
+    });
+    sizepopup.append(link);
+  });
+    
+	$('#my_size').unbind('click').bind('click', function(){
+		$('#color_popup').fadeOut(250);
+		$('#size_popup').fadeIn(500);
+	});
+	$('#my_size span').attr('data-size', '6');
 };
 
 drunkspotting.loading_start = function () {
-  
+  var loading = $('<div id="loading"><img src="/assets/img/loading.gif"/></div>');
+  loading.appendTo(document.body);
+  loading.fadeIn(250);
 };
 
 drunkspotting.loading_stop = function () {
-  
+  $('#loading').fadeOut(500, function(){
+	  $('#loading').remove();
+  })
 };
 
 drunkspotting.save_drawing = function(){
   // save
-  var imageData = $('#tools_sketch')[0].toDataURL();
+  drunkspotting.loading_start();
+  var imageData = $('#sketch')[0].toDataURL();
 
   $.ajax({
     url : '/upload',
@@ -116,7 +176,9 @@ drunkspotting.save_drawing = function(){
     },
     success : function(data) {
       if (data.id) {
-        $('#edit-panel').html('<a href="/">Click here to refresh and see your image</a>');
+      	$(document.body).removeClass('edit');
+      	drunkspotting.load_images();
+        drunkspotting.loading_stop();
       }
     },
     error : function (){}
